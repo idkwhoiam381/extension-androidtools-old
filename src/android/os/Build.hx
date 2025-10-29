fc08b8b121f2a2f28c399aec1cfd4a023b77734c
idkwhoiam381 <ismotass86@gmail.com>
2025-10-29 18:57:55 +0300

    Update Build.hx

--- src/android/os/Build.hx
+++ src/android/os/Build.hx
@@ -22,4 +22,31 @@ class Build {
 	 * The SDK version of the software currently running on this hardware device.
 	 */
 	public static var SDK_INT:Int = JNI.createStaticField("android/os/Build$VERSION", "SDK_INT", "I").get();
+	
+	public static final ODM_SKU:String = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? JNICache.createStaticField('android/os/Build', 'ODM_SKU',
+		'Ljava/lang/String;')
+		.get() : UNKNOWN;
+
+	/**
+	 * The SKU of the hardware (from the kernel command line).
+	 * The SKU is reported by the bootloader to configure system software features.
+	 * If no value is supplied by the bootloader, this is reported as ``UNKNOWN``.
+	 */
+	public static var SKU:String = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? JNICache.createStaticField('android/os/Build', 'SKU',
+		'Ljava/lang/String;')
+		.get() : UNKNOWN;
+
+	/**
+	 * The manufacturer of the device's primary system-on-chip.
+	 */
+	public static var SOC_MANUFACTURER:String = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? JNICache.createStaticField('android/os/Build',
+		'SOC_MANUFACTURER', 'Ljava/lang/String;')
+		.get() : UNKNOWN;
+
+	/**
+	 * The model name of the device's primary system-on-chip.
+	 */
+	public static var SOC_MODEL:String = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? JNICache.createStaticField('android/os/Build', 'SOC_MODEL',
+		'Ljava/lang/String;')
+		.get() : UNKNOWN;
 }
